413f38bc35f4cd745cbcfa5a0403a9e1604266f8
Theo Gregory <tjmg@outlook.com>
2022-08-05 10:36:33 +0200

    feat: exercise 4 - wraps the shoe grid

--- src/components/ShoeCard/ShoeCard.js
+++ src/components/ShoeCard/ShoeCard.js
@@ -61,7 +61,9 @@ const ImageWrapper = styled.div`
   position: relative;
 `;
 
-const Image = styled.img``;
+const Image = styled.img`
+  max-width: 100%;
+`;
 
 const Row = styled.div`
   font-size: 1rem;
--- src/components/ShoeGrid/ShoeGrid.js
+++ src/components/ShoeGrid/ShoeGrid.js
@@ -4,16 +4,28 @@ import styled from 'styled-components/macro';
 import SHOES from '../../data';
 import ShoeCard from '../ShoeCard';
 
+
 const ShoeGrid = () => {
   return (
     <Wrapper>
       {SHOES.map((shoe) => (
-        <ShoeCard key={shoe.slug} {...shoe} />
+        <CardWrapper>
+          <ShoeCard key={shoe.slug} {...shoe} />
+        </CardWrapper>
       ))}
     </Wrapper>
   );
 };
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 36px;
+`;
+
+const CardWrapper = styled.div`
+  flex: 1;
+  min-width: 216px;
+`
 
 export default ShoeGrid;
